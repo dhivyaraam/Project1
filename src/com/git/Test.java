ed52ebf2e1b31bc37b4212e436553cfe16c61e53
dhivyaraam <dhivyaramkumar315@gmail.com>
2024-04-20 13:29:00 +0530

    updated new code

--- src/com/git/Test.java
+++ src/com/git/Test.java
@@ -4,8 +4,12 @@ public class Test {
 private void test1() {
 	System.out.println("Git run successfully");
 }
+private void test2() {
+	System.out.println("new code");
+}
 public static void main(String[] args) {
 	Test t=new Test();
 	t.test1();
+	t.test2();
 }
 }
