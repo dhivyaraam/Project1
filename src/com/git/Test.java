5ec94ba9a7f861089d4f53c83fc06345c4eead95
dhivyaraam <dhivyaramkumar315@gmail.com>
2024-04-20 12:54:18 +0530

    new code

---
+++ src/com/git/Test.java
@@ -0,0 +1,11 @@
+package com.git;
+
+public class Test {
+private void test1() {
+	System.out.println("Git run successfully");
+}
+public static void main(String[] args) {
+	Test t=new Test();
+	t.test1();
+}
+}
